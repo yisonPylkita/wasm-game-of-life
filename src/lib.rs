b5bec46e1a00a586c4c5ed957a6ab34fb8bee52f
Wojciech Bartnik <yisonPylkita@gmail.com>
2020-01-04 16:35:19 +0100

    World init mock

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,19 @@ mod utils;
 
 use wasm_bindgen::prelude::*;
 
-// When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
-// allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+// // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
+// // allocator.
+// #[cfg(feature = "wee_alloc")]
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
+enum CellState {
+    Dead,
+    Alive,
 }
 
 #[wasm_bindgen]
-pub fn greet() {
-    alert("Hello there ^_^");
+pub fn init_world() {
+    // TODO: receive seed as an argument
+    // TODO: init world here
 }
